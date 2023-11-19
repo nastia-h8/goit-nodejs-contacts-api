341670760a92902e38285c8a4c3264784448611c
Anastasiia Haliara <an.haliara@gmail.com>
2023-11-19 19:13:31 +0200

    replace CRUD operations with Mongoose methods

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,18 +1,19 @@
 import express from "express";
 
 import contactsController from "../../controllers/contacts-controller.js";
-import { isEmptyBody } from "../../middlewars/index.js";
+import { isEmptyBody, isValidId } from "../../middlewars/index.js";
 import { validateBody } from "../../decorators/index.js";
 import {
   contactAddSchema,
   contactUpdateSchema,
+  contactFavoriteSchema,
 } from "../../schemas/contacts-schemas.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsController.getAllContacts);
 
-contactsRouter.get("/:id", contactsController.getContactById);
+contactsRouter.get("/:id", isValidId, contactsController.getContactById);
 
 contactsRouter.post(
   "/",
@@ -21,13 +22,21 @@ contactsRouter.post(
   contactsController.addContact
 );
 
-contactsRouter.delete("/:id", contactsController.removeContact);
+contactsRouter.delete("/:id", isValidId, contactsController.removeContact);
 
 contactsRouter.put(
   "/:id",
+  isValidId,
   isEmptyBody,
   validateBody(contactUpdateSchema),
   contactsController.updateContact
+);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(contactFavoriteSchema),
+  contactsController.updateStatusContact
 );
 
 export default contactsRouter;
